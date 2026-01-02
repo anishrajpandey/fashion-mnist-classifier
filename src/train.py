1e70f10ed551ad63fb1f8e07a481c5c6530a45c7
Anish <anishpandey021@gmail.com>
2026-01-01 22:22:04 -0600

    training models started

---
+++ src/train.py
@@ -0,0 +1,6 @@
+# this is where i plan to write training code for my machine learning model
+def train_model(data, labels):
+    # Placeholder for training logic
+    print("Training model with provided data and labels...")
+    # Here you would add code to define, compile, and train your ML model
+    pass
